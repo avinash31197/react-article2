99dde2e570738d4e603c79d085b066605fe5bc45
avinash31197 <avinash47016@gmail.com>
2022-10-28 00:17:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,164 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>this is todo react file</h1>
+      <p>
+        Usually, in an app, you have a lot of UI components, and each of them
+        has many different states. For an example, a basic button component
+        could have the following states: In a regular state, with a text label.
+        In the disabled mode. In a loading state. Usually, it’s hard to see
+        these states without running a sample app or some examples. Create React
+        App doesn’t include any tools for this by default, but you can add
+        Storybook for React (source) or React Styleguidist (source) to your
+        project. These are third-party tools that let you develop components and
+        see all their states in isolation from your app. Storybook for React
+        Demo You can also deploy your Storybook or style guide as a static app.
+        This way, everyone in your team can view and review different states of
+        UI components without starting a backend server or creating an account
+        in your app.
+      </p>
+      <p>
+        Usually, in an app, you have a lot of UI components, and each of them
+        has many different states. For an example, a basic button component
+        could have the following states: In a regular state, with a text label.
+        In the disabled mode. In a loading state. Usually, it’s hard to see
+        these states without running a sample app or some examples. Create React
+        App doesn’t include any tools for this by default, but you can add
+        Storybook for React (source) or React Styleguidist (source) to your
+        project. These are third-party tools that let you develop components and
+        see all their states in isolation from your app. Storybook for React
+        Demo You can also deploy your Storybook or style guide as a static app.
+        This way, everyone in your team can view and review different states of
+        UI components without starting a backend server or creating an account
+        in your app.
+      </p>
+      <p>
+        Usually, in an app, you have a lot of UI components, and each of them
+        has many different states. For an example, a basic button component
+        could have the following states: In a regular state, with a text label.
+        In the disabled mode. In a loading state. Usually, it’s hard to see
+        these states without running a sample app or some examples. Create React
+        App doesn’t include any tools for this by default, but you can add
+        Storybook for React (source) or React Styleguidist (source) to your
+        project. These are third-party tools that let you develop components and
+        see all their states in isolation from your app. Storybook for React
+        Demo You can also deploy your Storybook or style guide as a static app.
+        This way, everyone in your team can view and review different states of
+        UI components without starting a backend server or creating an account
+        in your app.
+      </p>
+      <p>
+        Usually, in an app, you have a lot of UI components, and each of them
+        has many different states. For an example, a basic button component
+        could have the following states: In a regular state, with a text label.
+        In the disabled mode. In a loading state. Usually, it’s hard to see
+        these states without running a sample app or some examples. Create React
+        App doesn’t include any tools for this by default, but you can add
+        Storybook for React (source) or React Styleguidist (source) to your
+        project. These are third-party tools that let you develop components and
+        see all their states in isolation from your app. Storybook for React
+        Demo You can also deploy your Storybook or style guide as a static app.
+        This way, everyone in your team can view and review different states of
+        UI components without starting a backend server or creating an account
+        in your app.
+      </p>
+      <p>
+        Usually, in an app, you have a lot of UI components, and each of them
+        has many different states. For an example, a basic button component
+        could have the following states: In a regular state, with a text label.
+        In the disabled mode. In a loading state. Usually, it’s hard to see
+        these states without running a sample app or some examples. Create React
+        App doesn’t include any tools for this by default, but you can add
+        Storybook for React (source) or React Styleguidist (source) to your
+        project. These are third-party tools that let you develop components and
+        see all their states in isolation from your app. Storybook for React
+        Demo You can also deploy your Storybook or style guide as a static app.
+        This way, everyone in your team can view and review different states of
+        UI components without starting a backend server or creating an account
+        in your app.
+      </p>
+
+      <p>
+        Usually, in an app, you have a lot of UI components, and each of them
+        has many different states. For an example, a basic button component
+        could have the following states: In a regular state, with a text label.
+        In the disabled mode. In a loading state. Usually, it’s hard to see
+        these states without running a sample app or some examples. Create React
+        App doesn’t include any tools for this by default, but you can add
+        Storybook for React (source) or React Styleguidist (source) to your
+        project. These are third-party tools that let you develop components and
+        see all their states in isolation from your app. Storybook for React
+        Demo You can also deploy your Storybook or style guide as a static app.
+        This way, everyone in your team can view and review different states of
+        UI components without starting a backend server or creating an account
+        in your app.
+      </p>
+      <p>
+        Usually, in an app, you have a lot of UI components, and each of them
+        has many different states. For an example, a basic button component
+        could have the following states: In a regular state, with a text label.
+        In the disabled mode. In a loading state. Usually, it’s hard to see
+        these states without running a sample app or some examples. Create React
+        App doesn’t include any tools for this by default, but you can add
+        Storybook for React (source) or React Styleguidist (source) to your
+        project. These are third-party tools that let you develop components and
+        see all their states in isolation from your app. Storybook for React
+        Demo You can also deploy your Storybook or style guide as a static app.
+        This way, everyone in your team can view and review different states of
+        UI components without starting a backend server or creating an account
+        in your app.
+      </p>
+
+      <p>
+        Usually, in an app, you have a lot of UI components, and each of them
+        has many different states. For an example, a basic button component
+        could have the following states: In a regular state, with a text label.
+        In the disabled mode. In a loading state. Usually, it’s hard to see
+        these states without running a sample app or some examples. Create React
+        App doesn’t include any tools for this by default, but you can add
+        Storybook for React (source) or React Styleguidist (source) to your
+        project. These are third-party tools that let you develop components and
+        see all their states in isolation from your app. Storybook for React
+        Demo You can also deploy your Storybook or style guide as a static app.
+        This way, everyone in your team can view and review different states of
+        UI components without starting a backend server or creating an account
+        in your app.
+      </p>
+
+      <p>
+        Usually, in an app, you have a lot of UI components, and each of them
+        has many different states. For an example, a basic button component
+        could have the following states: In a regular state, with a text label.
+        In the disabled mode. In a loading state. Usually, it’s hard to see
+        these states without running a sample app or some examples. Create React
+        App doesn’t include any tools for this by default, but you can add
+        Storybook for React (source) or React Styleguidist (source) to your
+        project. These are third-party tools that let you develop components and
+        see all their states in isolation from your app. Storybook for React
+        Demo You can also deploy your Storybook or style guide as a static app.
+        This way, everyone in your team can view and review different states of
+        UI components without starting a backend server or creating an account
+        in your app.
+      </p>
+
+      <p>
+        Usually, in an app, you have a lot of UI components, and each of them
+        has many different states. For an example, a basic button component
+        could have the following states: In a regular state, with a text label.
+        In the disabled mode. In a loading state. Usually, it’s hard to see
+        these states without running a sample app or some examples. Create React
+        App doesn’t include any tools for this by default, but you can add
+        Storybook for React (source) or React Styleguidist (source) to your
+        project. These are third-party tools that let you develop components and
+        see all their states in isolation from your app. Storybook for React
+        Demo You can also deploy your Storybook or style guide as a static app.
+        This way, everyone in your team can view and review different states of
+        UI components without starting a backend server or creating an account
+        in your app.
+      </p>
     </div>
   );
 }
